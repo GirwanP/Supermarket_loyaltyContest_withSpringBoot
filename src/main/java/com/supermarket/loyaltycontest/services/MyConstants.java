31c4ac004cebf099fc8156021439618590d94491
My PC <girwanpaudel111@gmail.com>
2019-06-16 13:54:02 +0545

    nothing changed autoformatted only

--- src/main/java/com/supermarket/loyaltycontest/services/MyConstants.java
+++ src/main/java/com/supermarket/loyaltycontest/services/MyConstants.java
@@ -5,5 +5,6 @@ public class MyConstants {
 	public static final int weeklyPoint=250;
 	public static final int monthlyPoint=750;
 	public static final int maxInactiveSessionDuration=500;
+	
 	public static final int dayLength=2*30000;//1 min=60000
 }
